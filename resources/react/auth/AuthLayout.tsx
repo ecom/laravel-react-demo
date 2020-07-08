1493ee12d6979fb72e25492e6800bae54503bfaa
Guo Yunhe <i@guoyunhe.me>
2020-07-08 11:44:33 +0300

    auth layout

---
+++ resources/react/auth/AuthLayout.tsx
@@ -0,0 +1,29 @@
+import React, { ReactNode } from "react";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+import Container from "@material-ui/core/Container";
+
+import Banner from "../Banner";
+
+const useStyles = makeStyles({
+    root: {},
+    content: {}
+});
+
+export interface AuthLayoutProps {
+    children?: ReactNode;
+}
+
+export default function AuthLayout({ children }: AuthLayoutProps) {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <Banner src="https://source.unsplash.com/YP2MNNId-Qs/1920x480" />
+            <Container maxWidth="xs">
+                <div className={classes.content}>{children}</div>
+            </Container>
+        </div>
+    );
+}
--- resources/react/auth/Login.tsx
+++ resources/react/auth/Login.tsx
@@ -6,15 +6,13 @@ import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import Checkbox from "@material-ui/core/Checkbox";
-import Container from "@material-ui/core/Container";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Link from "@material-ui/core/Link";
 import TextField from "@material-ui/core/TextField";
 
-import Banner from "../Banner";
-
 import { login } from "./service";
 import { withAuth, WithAuthProps } from "./AuthContext";
+import AuthLayout from "./AuthLayout";
 
 const useStyles = makeStyles({
     buttonRow: {
@@ -54,65 +52,58 @@ function Login({ user, setUser }: WithAuthProps) {
     }
 
     return (
-        <div>
-            <Banner src="https://source.unsplash.com/YP2MNNId-Qs/1920x480" />
-            <Container maxWidth="xs">
-                <form method="POST" action="/login" onSubmit={handleSubmit}>
-                    <Box my={3}>
-                        <TextField
-                            label="Email"
-                            name="email"
-                            fullWidth
-                            required
-                            type="email"
-                            value={email}
-                            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                                setEmail(e.target.value)
-                            }
-                            error={!!errors?.errors?.email}
-                            helperText={errors?.errors?.email?.join(" ")}
-                        />
-                    </Box>
-                    <Box my={3}>
-                        <TextField
-                            label="Password"
-                            name="password"
-                            fullWidth
-                            required
-                            type="password"
-                            value={password}
-                            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                                setPassword(e.target.value)
-                            }
-                        />
-                    </Box>
-                    <Box my={3}>
-                        <FormControlLabel
-                            control={
-                                <Checkbox
-                                    name="remember"
-                                    checked={remember}
-                                    onChange={() => setRemember(!remember)}
-                                />
-                            }
-                            label="Remember me"
-                        />
-                    </Box>
-                    <Box className={classes.buttonRow}>
-                        <Button
-                            type="submit"
-                            variant="contained"
-                            color="primary"
-                        >
-                            Login
-                        </Button>
-                        <Link component={RRLink} to="/password/reset">
-                            Forgot Your Password?
-                        </Link>
-                    </Box>
-                </form>
-            </Container>
-        </div>
+        <AuthLayout>
+            <form method="POST" action="/login" onSubmit={handleSubmit}>
+                <Box my={3}>
+                    <TextField
+                        label="Email"
+                        name="email"
+                        fullWidth
+                        required
+                        type="email"
+                        value={email}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                            setEmail(e.target.value)
+                        }
+                        error={!!errors?.errors?.email}
+                        helperText={errors?.errors?.email?.join(" ")}
+                    />
+                </Box>
+                <Box my={3}>
+                    <TextField
+                        label="Password"
+                        name="password"
+                        fullWidth
+                        required
+                        type="password"
+                        value={password}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                            setPassword(e.target.value)
+                        }
+                    />
+                </Box>
+                <Box my={3}>
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                name="remember"
+                                checked={remember}
+                                onChange={() => setRemember(!remember)}
+                            />
+                        }
+                        label="Remember me"
+                    />
+                </Box>
+                <Box className={classes.buttonRow}>
+                    <Button type="submit" variant="contained" color="primary">
+                        Login
+                    </Button>
+                    <Link component={RRLink} to="/password/reset">
+                        Forgot Your Password?
+                    </Link>
+                </Box>
+            </form>
+        </AuthLayout>
     );
 }
 
