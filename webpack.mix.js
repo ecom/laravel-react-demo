ffd16873ccbcaae6534355d9120828c0fdda2279
Guo Yunhe <i@guoyunhe.me>
2021-01-24 12:56:25 +0800

    update mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
 
-mix.ts('resources/react/index.tsx', 'public/js/app.js').extract().sourceMaps();
+mix.ts('resources/ts/index.tsx', 'public/js/app.js').react().extract().sourceMaps();
 
-mix.browserSync('192.168.10.10');
+//mix.browserSync('192.168.10.10');
